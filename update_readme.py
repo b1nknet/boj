0fe932950cc966f5d7660234318486e19616653e
b1nk <me@b1nk.net>
2025-06-13 18:35:26 +0900

    ^q^

--- update_readme.py
+++ update_readme.py
@@ -12,8 +12,9 @@ def get_header(handle):
     header += "# BOJ\n\n"
     header += "**백준 문제 풀이 저장소**\n\n"
     header += "![Github code size in bytes](https://img.shields.io/github/languages/code-size/b1nknet/boj?style=flat-square)\n\n"
-    header += f'<img style="height:60px" alt="" src="assets/tier/{get_solved_int(handle, "tier")}.svg">\n\n'
-    header += f'<h3>{handle}</h3>\n\n*( [solved.ac](https://solved.ac/{handle}) | [BOJ](https://acmicpc.net/user/{handle}) )*\n\n'
+    header += f'[![solved.ac 프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj={handle})](https://solved.ac/{handle})\n'
+    header += f'![solved.ac 잔디](http://mazandi.herokuapp.com/api?handle={handle}&theme=warm)\n\n'
+    header += f'*( [solved.ac](https://solved.ac/{handle}) | [BOJ](https://acmicpc.net/user/{handle}) )*\n\n'
     header += "rate: **" + str(get_solved_int(handle, "rating")) + "** | "
     header += "solved: **" + str(get_solved_int(handle, "solvedCount")) + "** | "
     header += "class: **" + str(get_solved_int(handle, "class")) + "**\n\n"
