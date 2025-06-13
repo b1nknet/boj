4b0d6982f59e2d17cd2ad3f80d1952a974c8892e
b1nk <me@b1nk.net>
2025-06-13 17:58:40 +0900

    ^q^

--- update_readme.py
+++ update_readme.py
@@ -68,7 +68,7 @@ def get_table():
     table += '|:---:|:---:|:---:|:---:|\n'
 
     for s in dir_list:
-        files = os.listdir(f"./{s}")
+        files = sorted(os.listdir(f"./{s}"))
 
         for filename in files:
             probId, fileExt = map(str, filename.split("."))
