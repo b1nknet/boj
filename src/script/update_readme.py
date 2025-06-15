e88f85ec6e802ac8562e5a9a155f17d229ca1bc9
b1nk <me@b1nk.net>
2025-06-15 12:59:09 +0900

    Use absolute path

--- src/script/update_readme.py
+++ src/script/update_readme.py
@@ -51,7 +51,7 @@ def get_table():
     table += '|:---:|:---:|:---:|:---:|\n'
 
     for s in dir_list:
-        files = sorted(os.listdir(f"../../{s}"))
+        files = sorted(os.listdir(f"/home/runner/work/boj/boj/{s}"))
 
         for filename in files:
             probId, fileExt = map(str, filename.split("."))
