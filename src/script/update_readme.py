b38df8852569bf8d47584065e4a14a76926da5f6
b1nk <me@b1nk.net>
2025-06-25 23:09:11 +0900

    .

--- src/script/update_readme.py
+++ src/script/update_readme.py
@@ -4,33 +4,29 @@ from datetime import datetime, timezone, timedelta
 baseurl = 'https://solved.ac/api/v3'
 
 def get_header(handle):
-    response = requests.get(f"{baseurl}/user/show", params={"handle": handle})
-    response.raise_for_status()
+    response = requests.get(f"{baseurl}/user/show?handle={handle}").raise_for_status()
     user_data = response.json()
 
-    rating = user_data['rating']
-    solvedCount = user_data['rating']
-    userClass = user_data['rating']
+    rating = str(user_data['rating'])
+    solvedCount = str(user_data['solvedCount'])
+    userClass = str(user_data['class'])
 
-    header = '<div align="center">\n\n'
-    header += "# BOJ\n\n"
-    header += "**백준 문제 풀이 저장소**\n\n"
-    header += "![Github code size in bytes](https://img.shields.io/github/languages/code-size/b1nknet/boj?style=flat-square)\n\n"
-    header += f'[![solved.ac 프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj={handle})](https://solved.ac/{handle})\n'
-    header += f'![solved.ac 잔디](http://mazandi.herokuapp.com/api?handle={handle}&theme=dark)\n\n'
-    header += f'*( [solved.ac](https://solved.ac/{handle}) | [BOJ](https://acmicpc.net/user/{handle}) )*\n\n'
-    header += "rate: **" + str(rating) + "** | "
-    header += "solved: **" + str(solvedCount) + "** | "
-    header += "class: **" + str(userClass) + "**\n\n"
-    header += "업데이트: "
-    header += datetime.now(timezone(timedelta(hours=9))).strftime("%y.%m.%d. %H:%M:%S")
-    header += " (KST)\n\n"
-    header += '</div>\n'
+    header = '<div align="center">\n\n' \
+        + "# BOJ\n\n" \
+        + "**백준 문제 풀이 저장소**\n\n" \
+        + "![Github code size in bytes](https://img.shields.io/github/languages/code-size/b1nknet/boj?style=flat-square)\n\n" \
+        + f'[![solved.ac 프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj={handle})](https://solved.ac/{handle})\n' \
+        + f'![solved.ac 잔디](http://mazandi.herokuapp.com/api?handle={handle}&theme=dark)\n\n' \
+        + f'*( [solved.ac](https://solved.ac/{handle}) | [BOJ](https://acmicpc.net/user/{handle}) )*\n\n' \
+        + "rate: **" + rating + "** | " \
+        + "solved: **" + solvedCount + "** | " \
+        + "class: **" + userClass + "**\n\n" \
+        + "업데이트: " + datetime.now(timezone(timedelta(hours=9))).strftime("%y.%m.%d. %H:%M:%S") + " (KST)\n\n" \
+        + '</div>\n' 
     return header
 
 def get_prob_list():
     prob_list = []
-
     dir_list = [f"{format(i, '02')}xxx" for i in range(1, 34)]
 
     for s in dir_list:
@@ -41,19 +37,18 @@ def get_prob_list():
             prob_list.append({"id": probId, "ext": fileExt, "file_location": f'{s}/{filename}'})
 
     prob_id_list = [prob['id'] for prob in prob_list]
-    
     fetchParam = ''
     for id in prob_id_list:
         fetchParam += id
         if prob_id_list.index(id) != len(prob_id_list) - 1:
             fetchParam += "%2C"
 
-    response = requests.get(f"{baseurl}/problem/lookup?problemIds={fetchParam}")
-    response.raise_for_status()
+    response = requests.get(f"{baseurl}/problem/lookup?problemIds={fetchParam}").raise_for_status()
+    probs_data = response.json()
     for p in prob_list:
-        idx = prob_list.index(p)
-        p['title'] = response.json()[idx]['titles'][0]['title']
-        p['level'] = response.json()[idx]['level']
+        prob_data = probs_data[prob_list.index(p)]
+        p['title'] = prob_data['titles'][0]['title']
+        p['level'] = prob_data['level']
 
     return prob_list
 
@@ -70,7 +65,6 @@ def get_table():
     }
 
     prob_list = get_prob_list()
-
     for p in prob_list:
         table += '| ' + p['id'] + ' | ' + p['title'] + ' | <img style="height:30px;" src="src/tier/' + str(p['level']) + '.svg"> | [' + ext[p['ext']] + '](./'+ p['file_location'] +') |\n'
 
