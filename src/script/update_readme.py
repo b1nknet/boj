910bb3d7250fccf5df3a619e745f3421026940f4
b1nk <me@b1nk.net>
2025-06-25 23:34:20 +0900

    .

--- src/script/update_readme.py
+++ src/script/update_readme.py
@@ -12,41 +12,29 @@ def get_header(handle):
     solvedCount = str(user_data['solvedCount'])
     userClass = str(user_data['class'])
 
-    header = '<div align="center">\n\n' \
-        + "# BOJ\n\n" \
-        + "**백준 문제 풀이 저장소**\n\n" \
+    header = '<div align="center">\n\n# BOJ\n\n**백준 문제 풀이 저장소**\n\n' \
         + "![Github code size in bytes](https://img.shields.io/github/languages/code-size/b1nknet/boj?style=flat-square)\n\n" \
         + f'[![solved.ac 프로필](http://mazassumnida.wtf/api/v2/generate_badge?boj={handle})](https://solved.ac/{handle})\n' \
         + f'![solved.ac 잔디](http://mazandi.herokuapp.com/api?handle={handle}&theme=dark)\n\n' \
         + f'*( [solved.ac](https://solved.ac/{handle}) | [BOJ](https://acmicpc.net/user/{handle}) )*\n\n' \
-        + "rate: **" + rating + "** | " \
-        + "solved: **" + solvedCount + "** | " \
-        + "class: **" + userClass + "**\n\n" \
+        + f"rate: **{rating}** | solved: **{solvedCount}** | class: **{userClass}**\n\n" \
         + "업데이트: " + datetime.now(timezone(timedelta(hours=9))).strftime("%y.%m.%d. %H:%M:%S") + " (KST)\n\n" \
         + '</div>\n' 
     return header
 
 def get_prob_list():
     prob_list = []
-    dir_list = [f"{format(i, '02')}xxx" for i in range(1, 34)]
-
-    for s in dir_list:
+    for s in [f"{format(i, '02')}xxx" for i in range(1, 34)]:
         files = sorted(os.listdir(f"./{s}"))
         
         for filename in files:
             probId, fileExt = map(str, filename.split('.'))
             prob_list.append({"id": probId, "ext": fileExt, "file_location": f'{s}/{filename}'})
 
-    prob_id_list = [prob['id'] for prob in prob_list]
-    fetchParam = ''
-    for id in prob_id_list:
-        fetchParam += id
-        if prob_id_list.index(id) != len(prob_id_list) - 1:
-            fetchParam += "%2C"
-
-    response = requests.get(f"{baseurl}/problem/lookup?problemIds={fetchParam}")
+    response = requests.get(f"{baseurl}/problem/lookup?problemIds={"%2C".join([prob['id'] for prob in prob_list])}")
     response.raise_for_status()
     probs_data = response.json()
+    
     for p in prob_list:
         prob_data = probs_data[prob_list.index(p)]
         p['title'] = prob_data['titles'][0]['title']
@@ -66,8 +54,7 @@ def get_table():
         "py": "Python"
     }
 
-    prob_list = get_prob_list()
-    for p in prob_list:
+    for p in get_prob_list():
         table += '| ' + p['id'] + ' | ' + p['title'] + ' | <img style="height:30px;" src="src/tier/' + str(p['level']) + '.svg"> | [' + ext[p['ext']] + '](./'+ p['file_location'] +') |\n'
 
     table += '\n</div>'
